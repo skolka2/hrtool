40dc9e1ef74e8549e019d0780a434a59fae60e05
Honza Koren <jan.koren@socialbakers.com>
2014-07-30 13:19:34 +0200

    some examples

--- client/js/test.js
+++ client/js/test.js
@@ -1,11 +1,49 @@
-var Base = function(){
+var App = function(){
 	this.socket = io.connect();
 }
 
-Base.prototype.getSumFromBackend = function(numbers){
+App.prototype.getSumFromBackend = function(numbers){
 	this.socket.emit('sum',numbers);
 	this.socket.on('resp-sum', function(data){
 		console.log(data);
 	});
-
 }
+
+
+var First = function(number){
+	this.number = number+number;
+	this.type = 'first';
+}
+
+First.prototype.getType = function(){
+	return this.type + ' ' + this.number;
+}
+
+First.prototype.sayHi = function(){
+	console.log('hi');
+	// alert('hi');
+}
+
+First.prototype.sayNo = function(){
+	console.log('no');
+}
+
+var Second = function(number){
+	First.call(this, number);
+	this.type = 'second';
+}
+
+Second.prototype = new First();
+Second.prototype.constructor = Second;
+
+Second.prototype.getType = function(){
+	console.log('all');
+}
+
+
+var f = new First(10);
+console.log(f.getType());
+var s = new Second(15);
+console.log(s.getType());
+s.sayNo();
+s.sayHi();
