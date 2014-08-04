d69e7004bac0a406e67c9215bbdf3f3105aaa491
ZibbyKarel <zibbykarel@gmail.com>
2014-08-04 12:04:57 +0200

    funkcni express.io + easy-pg

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var config = require('./config.json');                                          //configuration settings
 
-app.use(express.static(__dirname + '/public'));
+(function(){
+    var epg = require('easy-pg');
+    var dbClient = epg(config.conString);                                       //database client for sending queries
+    var express = require('express.io');
+    var app = express();
 
-app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html');
-});
+    app.http().io();
+    app.listen(config.port);
+    console.log('Server listens on port ' + config.port);
 
-io.on('connection', function (socket) {
+    app.use(express.static(__dirname + '/public'));
 
-	socket.on('sum', function (data) {
-		sum = 0;
-		for (var i = 0; i < data.length; i++) sum += data[i]
-		socket.emit('sum', sum);
-	});
-
-});
-
-var port = 8080;
-server.listen(port);
-console.log('Server listens on ' + port)
+    app.get('/', function (req, res) {
+            res.sendfile(__dirname + '/index.html');
+    });
+       
+       
+    
+    
+    
+    
+    
+    
+    
+    
+//SOCKETS EVENTS
+    
+    //connected to database
+    dbClient.on('ready', function(){
+        console.log('>> Successfully connected to database');
+    });
+    
+    //disconnected from database
+    dbClient.on('end', function(){
+        console.log('>> Successfully disconnected to database');
+    });
+    
+    //database error
+    dbClient.on('error', function(err){
+        console.log('>> Database client error:\n' + err);
+    });
+    
+    //client was connected to the server
+    app.io.route('client connected', function(req) {
+        req.io.respond(true);
+        console.log('>> A client was connected');
+    });
+    
+})();
