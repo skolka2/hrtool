4c8cc9b811601b5ab77cecfed630764617d4b611
Frantisek Kolenak <fkolenak@students.zcu.cz>
2014-08-05 12:52:01 +0200

    Code review - fixed

--- views/baseView.js
+++ views/baseView.js
@@ -1,32 +1,28 @@
-(function(){
-	var baseView;
+
+var baseView;
 	//TODO get elements from helper, inherit from observableComponent
-	baseView = (function(){
-		function baseView(){
-			this.mainWrapper = "main-wrapper";
+baseView = (function(){
+	
+	function baseView(){
+		baseView.mainWrapper = "main-wrapper";
+	}
+
+	baseView.prototype.render = function(){
+		this.base = document.getElementById(baseView.mainWrapper);
+		if(typeof this.base != 'undefined'){ //checks if, in body, exists main-wrapper
+			var childs = this.base.childNodes;
+			var length = childs.length;
+			for(var i = 0;i < length;i++){
+				
+				this.base.removeChild(childs[0]);	//Delete all content from main-wrapper
+			}
 		}
-
-		baseView.prototype.render = function(){
-			this.base = document.getElementById(this.mainWrapper);
-			if(typeof this.base != 'undefined'){ //checks if, in body, exists main-wrapper
-				while(true){
-					child = this.base.childNodes[0];
-					if(typeof child != 'undefined'){
-						this.base.removeChild(child);	//Delete all content from main-wrapper
-					}
-					else {
-						break;
-					}
-				}
-			}
-			else{	//Adds main-wrapper
-				var body = document.getElementsByTagName("body")[0];
-				var div = document.createElement("div");
-				div.id = this.mainWrapper;
-				body.appendChild(div);
-			}
-		};
-		return baseView;
-	})(/*observableComp*/);
-
-}.call(this);
+		else{	//Adds main-wrapper
+			var body = document.getElementsByTagName("body")[0];
+			var div = document.createElement("div");
+			div.id = baseView.mainWrapper;
+			body.appendChild(div);
+		}
+	};
+	return baseView;
+})(/*observableComp*/);
--- views/defaultView.js
+++ views/defaultView.js
@@ -1,25 +1,24 @@
-(function(){
-	var defaultView, baseView;
-	var __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-	
-	baseView = require("./baseView.js");
 
-	defaultView = (function(_super) {
-	    __extends(viewUser, _super);
+var defaultView, baseView;
 
-		function defaultView(){
-			return baseView.__super__.constructor.apply(this, arguments);
-		}
 
-		defaultView.prototype.render = function(){
-			baseView.__super__.render.apply(this, arguments);
-			var mainWrapper = document.getElementById(this.mainWrapper);
-			var div = document.createElement("div");
-			div.id = "defaultView";
-			div.innerText = "This is default view, you should not be seeing this (you should see other views instead).";
-			mainWrapper.appendChild(div);
-		};
-		return defaultView;
-	})(baseView);
+//baseView = require("./baseView.js");
+baseView = baseView;
 
-}.call(this);
+defaultView = (function(_super) {
+
+	function defaultView(){
+		this.super = baseView;
+		this.super.call(this,null);
+	}
+
+	defaultView.prototype.render = function(){
+		this.super.prototype.render.apply(this, arguments);
+		var mainWrapper = document.getElementById(baseView.mainWrapper);
+		var div = document.createElement("div");
+		div.id = "defaultView";
+		div.innerText = "This is default view, you should not be seeing this (you should see other views instead).";
+		mainWrapper.appendChild(div);
+	};
+	return defaultView;
+})(baseView);
