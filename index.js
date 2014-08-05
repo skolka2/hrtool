9234db9a853112d68243864f03cbca2e75ed7b72
ZibbyKarel <zibbykarel@gmail.com>
2014-08-05 14:49:30 +0200

    upraveno nastveni session

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.http().io();
 app.listen(config.port);
 console.log('Server listens on port ' + config.port);
 
+app.use(express.cookieParser());
 app.use(express.session({secret: 'monkey'}));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    console.log(req.session.startTime);
     res.sendfile(__dirname + '/index.html');
 });
 
