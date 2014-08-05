e25c285acb7d827e2f370538b2b5a12ffcb354c8
Marek Simunek <marek-simunek@seznam.cz>
2014-08-05 13:56:38 +0200

    mediator edited

--- public/js/models/mediator.js
+++ public/js/models/mediator.js
@@ -2,17 +2,20 @@
 var mediator =  function() {
 	this.super = Observer;
 
-} 
+}
+
+mediator.prototype = Observer;
+mediator.prototype.constructor = mediator; 
 
 /*
   * @returns {object} singleton socket
-*/
-mediator.prototype.getSocket = function() {
-	if (mediator.socket == null) {
-		mediator.socket = io.connect();
-	}
-	return mediator.socket;
-};
+  */
+  mediator.prototype.getSocket = function() {
+  	if (mediator.socket == null) {
+  		mediator.socket = io.connect();
+  	}
+  	return mediator.socket;
+  };
 
 /* call backend to retrieve data
 * @param {object} callee 
@@ -23,21 +26,17 @@ mediator.prototype.getSocket = function() {
 */
 mediator.prototype.loadData = function(callee, endpoint, params, type, transform){
 	var self = this;
-	this.getSocket().emit(endpoint, params, (function() {
-		return function(err) {
-			if (err != null) {
-				console.log("error", err);
-			}
-		};
+	this.getSocket().emit(endpoint, params, function( err) {	
+		if (err != null) {
+			console.log("error", err);
+		}
 	};
 
-	this.getSocket().on(endpoint + 'Resp', (function(data) {
-		return function(data) {
-			if (transform != null) {
-				data = transform(data);
-			}
-			self.fire(type, data, callee,);
-		};
+	this.getSocket().on(endpoint, params, function(data) {
+		if (transform != null) {
+			data = transform(data);
+		}
+		self.fire(type, data, callee);
 	};
 };
 
