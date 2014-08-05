7c71280c5fe4965df85e8de33a55889afdd81872
Vlada47 <laznickav@hotmail.cz>
2014-08-05 16:10:00 +0200

    added DROP IF EXISTS statements and corrected syntax and typos in code

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,5 +1,6 @@
 --Creating tables for the database with NOT NULL and UNIQUE conditions on specified columns.
 
+DROP TABLE IF EXISTS tasks CASCADE;
 CREATE TABLE tasks
 (
 	id_task SERIAL PRIMARY KEY,
@@ -13,6 +14,7 @@ CREATE TABLE tasks
 	id_buddy INTEGER
 );
 
+DROP TABLE IF EXISTS default_tasks CASCADE;
 CREATE TABLE default_tasks
 (
 	id_default_task SERIAL PRIMARY KEY,
@@ -22,29 +24,33 @@ CREATE TABLE default_tasks
 	id_department INTEGER
 );
 
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users
 (
 	id_user SERIAL PRIMARY KEY,
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
 	email TEXT NOT NULL UNIQUE,
-	id_role INTEGER,
-	id_buddy INTEGER,
+	id_user_role INTEGER,
+	id_buddy INTEGER
 );
 
+DROP TABLE IF EXISTS user_roles CASCADE;
 CREATE TABLE user_roles
 (
 	id_user_role SERIAL PRIMARY KEY,
 	title TEXT NOT NULL
 );
 
+DROP TABLE IF EXISTS users_teams CASCADE;
 CREATE TABLE users_teams
 (
 	id_user INTEGER NOT NULL,
 	id_team INTEGER NOT NULL,
-	team_admin BOOLEAN DEFAULT FALSE
+	is_admin BOOLEAN DEFAULT FALSE
 );
 
+DROP TABLE IF EXISTS teams CASCADE;
 CREATE TABLE teams
 (
 	id_team SERIAL PRIMARY KEY,
@@ -52,12 +58,14 @@ CREATE TABLE teams
 	id_department INTEGER NOT NULL
 );
 
+DROP TABLE IF EXISTS departments CASCADE;
 CREATE TABLE departments
 (
 	id_department SERIAL PRIMARY KEY,
 	title TEXT NOT NULL
 );
 
+DROP TABLE IF EXISTS department_roles CASCADE;
 CREATE TABLE department_roles
 (
 	id_department_role SERIAL PRIMARY KEY,
@@ -66,6 +74,8 @@ CREATE TABLE department_roles
 );
 
 -- Creating primary key constraint on user_teams table.
+ALTER TABLE users_teams
+DROP CONSTRAINT IF EXISTS pk_user_team;
 
 ALTER TABLE users_teams
 ADD CONSTRAINT pk_user_team PRIMARY KEY (id_user, id_team);
@@ -74,17 +84,26 @@ ADD CONSTRAINT pk_user_team PRIMARY KEY (id_user, id_team);
 
 -- Foreign key from ID_USER column of TASKS table to ID column from USERS table.
 ALTER TABLE tasks
+DROP CONSTRAINT IF EXISTS fk_tasks_id_user;
+
+ALTER TABLE tasks
 ADD CONSTRAINT fk_tasks_id_user
-FOREIGN KEY (id_users)
-REFERENCES users(id_users);
+FOREIGN KEY (id_user)
+REFERENCES users(id_user);
 
 -- Foreign key from ID_BUDDY column of TASKS table to ID column from USERS table.
 ALTER TABLE tasks
+DROP CONSTRAINT IF EXISTS fk_tasks_id_buddy;
+
+ALTER TABLE tasks
 ADD CONSTRAINT fk_tasks_id_buddy
 FOREIGN KEY (id_buddy)
-REFERENCES users(id_users);
+REFERENCES users(id_user);
 
 -- Foreign key from ID_TEAM column of DEFAULT_TASKS table to ID column from TEAMS table.
+ALTER TABLE default_tasks
+DROP CONSTRAINT IF EXISTS fk_default_tasks_id_team;
+
 ALTER TABLE default_tasks
 ADD CONSTRAINT fk_default_tasks_id_team
 FOREIGN KEY (id_team)
@@ -92,17 +111,26 @@ REFERENCES teams(id_team);
 
 -- Foreign key from ID_DEPARTMENT column of DEFAULT_TASKS table to ID column from DEPARTMENTS table.
 ALTER TABLE default_tasks
+DROP CONSTRAINT IF EXISTS fk_default_tasks_id_department;
+
+ALTER TABLE default_tasks
 ADD CONSTRAINT fk_default_tasks_id_department
 FOREIGN KEY (id_department)
 REFERENCES departments(id_department);
 
--- Foreign key from ID_ROLE column of USERS table to ID column from ROLES table.
+-- Foreign key from ID_USER_ROLE column of USERS table to ID column from USER_ROLES table.
+ALTER TABLE users
+DROP CONSTRAINT IF EXISTS fk_users_id_role;
+
 ALTER TABLE users
 ADD CONSTRAINT fk_users_id_role
-FOREIGN KEY (id_role)
-REFERENCES role(id_role);
+FOREIGN KEY (id_user_role)
+REFERENCES user_roles(id_user_role);
 
 -- Foreign key from ID_BUDDY column of USERS table to ID column from USERS table.
+ALTER TABLE users
+DROP CONSTRAINT IF EXISTS fk_users_id_buddy;
+
 ALTER TABLE users
 ADD CONSTRAINT fk_users_id_buddy
 FOREIGN KEY (id_buddy)
@@ -110,17 +138,26 @@ REFERENCES users(id_user);
 
 -- Foreign key from ID_USER column of USERS_IN_TEAMS table to ID column from USERS table.
 ALTER TABLE users_teams
+DROP CONSTRAINT IF EXISTS fk_users_teams_id_user;
+
+ALTER TABLE users_teams
 ADD CONSTRAINT fk_users_teams_id_user
 FOREIGN KEY (id_user)
 REFERENCES users(id_user);
 
 -- Foreign key from ID_TEAM column of USERS_IN_TEAMS table to ID column from TEAMS table.
 ALTER TABLE users_teams
+DROP CONSTRAINT IF EXISTS fk_users_teams_id_team;
+
+ALTER TABLE users_teams
 ADD CONSTRAINT fk_users_teams_id_team
 FOREIGN KEY (id_team)
-REFERENCES team(id_team);
+REFERENCES teams(id_team);
 
 -- Foreign key from ID_DEPARTMENT column of TEAMS table to ID column from DEPARTMENTS table.
+ALTER TABLE teams
+DROP CONSTRAINT IF EXISTS fk_teams_id_department;
+
 ALTER TABLE teams
 ADD CONSTRAINT fk_teams_id_department
 FOREIGN KEY (id_department)
@@ -128,6 +165,9 @@ REFERENCES departments(id_department);
 
 -- Foreign key from ID_DEPARTMENT column of DEPARTMENT_ROLES table to ID column from DEPARTMENTS table.
 ALTER TABLE department_roles
+DROP CONSTRAINT IF EXISTS fk_department_roles_id_department;
+
+ALTER TABLE department_roles
 ADD CONSTRAINT fk_department_roles_id_department
 FOREIGN KEY (id_department)
-REFERENCES departments(id_department)
+REFERENCES departments(id_department);
