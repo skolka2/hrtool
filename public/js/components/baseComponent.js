d7d989136cc42d57d92c415e66bf3820d352a23a
Frantisek Kolenak <fkolenak@students.zcu.cz>
2014-08-07 17:23:51 +0200

    Fixed some problems and warnings.

--- public/js/components/baseComponent.js
+++ public/js/components/baseComponent.js
@@ -4,12 +4,15 @@ var ComponentBase;
 
 //Default constructor
 ComponentBase = function(){
+	this.super = ObservableComponent;
+	this.super.prototype.constructor.apply(this);
 	this.childs = {};
 	this.element = null;
 	this.rendered = false;
-	ComponentBase.componentID = 0;
-	ComponentBase.mainWrapper = "main-wrapper";
 }
+
+ComponentBase.prototype = new ObservableComponent();
+ComponentBase.prototype.constructor = ComponentBase;
 
 //Insert child component
 ComponentBase.prototype.addChild = function (name, component, wraper){
@@ -34,30 +37,32 @@ ComponentBase.prototype.removeChild = function (name){
 	}
 }
 
-ComponentBase.prototype.destroyFromDOM = function (){
+ComponentBase.prototype.removeFromDOM = function (){
 	//check if its rendered
 	if(this.rendered){
-		var parentElement = this.getElement().parentElement; //Since I know parent element of this node, i can delete it completely(itself) from DOM
-		parentElement.removeChild(this.getElement());
+		for(child in this.childs){
+			this.childs[child].component.removeFromDOM();
+			this.element.remove();
+		}
+		
+		this.rendered = false;
 	}
-	else{
-		console.log("You cannnot delete this element from DOM, because its not rendered yet.");
-	}
+	
 }
 
 //Delete elements of component including listeners
 ComponentBase.prototype.destroy = function (){
 	//check if its rendered
-	this.destroyFromDOM();
-	if(this.rendered){
-		//TODO After obs is done remove from obs(listeners)
-		for(name in this.childs){
-			
-			this.childs[name].component.destroy();
-			delete this.childs[name];
-			//Remove from obs
-		}
+	this.removeFromDOM();
+	
+	//TODO After obs is done remove from obs(listeners)
+	for(name in this.childs){
+		
+		this.childs[name].component.destroy();
+		delete this.childs[name];
+		//Remove from obs
 	}
+	
 }
 
 // Prepare element
@@ -80,14 +85,22 @@ ComponentBase.prototype.render = function (wrapper){
 	var element = this.getElement();
 	wrapper = this.getWrapper(wrapper);
 	var parrent = wrapper;
-	element.parentNode = parrent;
-
 	parrent.appendChild(element);
 
 	this.rendered = true;
 
 	var child , parrentOfChild;
 	parrentOfChild = element;
+	/*var parrentWrapper = document.getElementById(wrapper.id);
+	if(parrentWrapper != null){
+		parrentOfChild = parrentWrapper;
+	}
+	else {
+		parrentWrapper = document.getElementsByName(wrapper.className)[0];
+		if(parrentWrapper!=null){
+			parrentOfChild = parrentWrapper;
+		}
+	}*/
 	for(name in this.childs){
 		child = this.childs[name];
 		child.component.render(parrentOfChild);
@@ -97,7 +110,9 @@ ComponentBase.prototype.render = function (wrapper){
 //Check if wrapper exists. If wrapper doesnt exists create general wrapper
 ComponentBase.prototype.getWrapper = function(wrapper){
 	if(wrapper == null){		
-		return document.getElementById("main-wrapper");
+		return document.getElementById(ComponentBase.mainWrapper);
 	}
 	return wrapper;
 }
+
+ComponentBase.mainWrapper = "main-wrapper";
