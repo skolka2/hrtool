bf7690dfbae3cd502badc04244b1352dafbba349
onevladimir <onevladimir@gmail.com>
2014-08-09 11:02:18 +0200

    opt

--- public/js/components/loginBar.js
+++ public/js/components/loginBar.js
@@ -1,27 +1,10 @@
 ﻿var ComponentLoginBar = function () {
     this.super = ComponentBase;
     this.super.call(this);
-    this.type = 'loginBar';
+    this.helper = helper;
 }
 ComponentLoginBar.prototype = new ComponentBase();
 ComponentLoginBar.prototype.constructor = ComponentLoginBar;
-
-ComponentLoginBar.prototype.addLoginBar = function () {
-    var div = document.createElement("div");
-    var userData = this.getUserData('012345');
-    var ahref;
-    div.id = "LoginBar";
-    //create ahref buddy 
-    ahref = ComponentLoginBar.prototype.createAhref("loginBar_buddy", userData.buddyFirstName + " " + userData.buddySurName, window.location + "User/user=" + userData.buddyId);
-    div.appendChild(ahref);
-    //create ahref Login name
-    ahref = ComponentLoginBar.prototype.createAhref("loginBar_name", userData.firstName + " " + userData.surName, window.location + "User/user=" + userData.id);
-    div.appendChild(ahref);
-    //create logout button
-    ahref = ComponentLoginBar.prototype.createButton("login_btn", "Logout");
-    div.appendChild(ahref);
-    return div;
-}
 
 ComponentLoginBar.prototype.getUserData = function (userID) {
     var data = {
@@ -37,28 +20,19 @@ ComponentLoginBar.prototype.getUserData = function (userID) {
 }
 
 ComponentLoginBar.prototype.createDom = function () {
-    this.element = ComponentLoginBar.prototype.addLoginBar();
-}
-
-//shut be in helper?
-ComponentLoginBar.prototype.createButton = function(id, textNode) {
-    var btn = document.createElement("button");
-    var t = document.createTextNode(textNode);
-    btn.id = id;
-    btn.setAttribute('type', 'button');
-    btn.setAttribute('name', textNode);
-    btn.setAttribute('value', textNode);
-    btn.appendChild(t);
-    return btn;
-}
-
-//shut be in helper?
-ComponentLoginBar.prototype.createAhref = function (id, textNode, href) {
-    var a = document.createElement('a');
-    var text = document.createTextNode(textNode);
-    a.appendChild(text);
-    a.title = text.textContent;
-    a.id = id;
-    a.href = href;
-    return a;
+    var div = document.createElement("div");
+    var userData = this.getUserData('012345');
+    var elem;
+    div.id = "LoginBar";
+    
+    //create ahref buddy 
+    elem = helper.elementsFunctions.getElement('<a id="loginBar_buddy" href="' + window.location + "User/user=" + userData.buddyId + '">' + userData.buddyFirstName + " " + userData.buddySurName + '</a>');
+    div.appendChild(elem);
+    //create ahref Login name
+    elem = helper.elementsFunctions.getElement('<a id="loginBar_name" href="' + window.location + "User/user=" + userData.id + '">' + userData.firstName + " " + userData.surName + '</a>');
+    div.appendChild(elem);
+    //create logout button
+    elem = helper.elementsFunctions.getElement('<button id="login_btn" type="button">Logout</button>');
+    div.appendChild(elem);
+    this.element = div;
 }
