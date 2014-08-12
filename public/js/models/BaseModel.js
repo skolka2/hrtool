eb89b28b9492ca0741d3cbbb2cdc133ff58ac1c5
mareksimunek <marek-simunek@seznam.cz>
2014-08-12 09:59:54 +0200

    Update BaseModel.js
    callee param removed from loadData

--- public/js/models/BaseModel.js
+++ public/js/models/BaseModel.js
@@ -8,14 +8,13 @@ ModelBase.prototype = new ObservableComponent();
 ModelBase.prototype.constructor = ModelBase; 
 
 /* loadData using medaitor 
-* @param {object} callee 
 * @param {string} endpoint
 * @param {object} params parametres
 * @param {type} type type of event
 * @param {object} transform transformation of loaded data
 */
-ModelBase.prototype.loadData = function(callee, endpoint, params, type, transform) {
-	this.mediator.loadData(callee, endpoint, params, type, transform);
+ModelBase.prototype.loadData = function(endpoint, params, type, transform) {
+	this.mediator.loadData(endpoint, params, type, transform);
 };
 
 
