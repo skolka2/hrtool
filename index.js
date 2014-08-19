595a8c95b5a8017972d7099402fb7bd30834981a
onevladimir <onevladimir@gmail.com>
2014-08-19 10:10:03 +0200

    NavBarAdd

--- index.js
+++ index.js
@@ -98,6 +98,11 @@ app.get('/auth/google/return',
     failureRedirect: '/'
 }));
 
+//logout
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
 
 
 
--- public/js/views/viewHome.js
+++ public/js/views/viewHome.js
@@ -1,13 +1,15 @@
 var ViewHome = function(){
     this.helper = helper;
     this.component = new ComponentTest();
+    this.componentNavBar = new ComponentNavBar();
 }
 
-ViewHome.prototype.render = function(){
+ViewHome.prototype.render = function () {
     var el = document.createElement('div');
     el.innerHTML = 'first content';
     var body = document.getElementsByTagName('body')[0];
     body.appendChild(el);
+    this.componentNavBar.render();
     this.component.render(body);
 
 }
